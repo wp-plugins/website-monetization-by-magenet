320c640442edeb415f8e7b52bb73ef2d5f6faa98
MageNet <MageNet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 14:05:32 +0000

    = 1.0.12 = * Fixed the issue with installing links at the homepages * Fix some other small bugs
    git-svn-id: https://plugins.svn.wordpress.org/website-monetization-by-magenet/tags/1.0.12@999168 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ view-settings.php
@@ -0,0 +1,58 @@
+<div class="wrap">
+    <h2>Website Monetization by MageNet</h2>
+    <p>By installing WordPress plugin, you allow MageNet to display paid contextual ads on pages where where you grant permission via Pages options.</p>
+    <p>Ad placement and removal process takes 3 hours. Please be patient.</p>
+    <div class="tool-box">
+        <form id="form-id" method="post" action="">
+            <input type="hidden" name="seoplugin" id="seoplugin" value="1">
+            <input type="checkbox" name="seoparam" id="seoparam" onClick="document.getElementById('form-id').submit();" <?php if ($is_active_seo_plugin=='on') echo "CHECKED"; ?> ><span>I have "Wordpress SEO" plugin by Yoast activated on my website. Note: check this box to avoid errors of MageNet Monetization plugin caused by this SEO plugin.</span>
+            <?php echo $plugin_result_text; ?>
+        </form>
+    </div>
+    <div class="tool-box">
+        <h2 class="title">MageNet Key:</h2>
+        <form method="post" action="">
+            <input type="text" name="key" value="<?php echo $magenet_key;?>" />
+            <input type="submit" name="submit" value="Save" />
+            <?php echo $result_text; ?>
+        </form>
+    </div>
+    <?if ($magenet_key) { ?>
+    <div class="tool-box">
+        <h3 class="title">Active Ads:</h2>
+        <table class="widefat">	 
+            <thead>
+                <tr class="table-header">
+                    <th>Page URL</th>
+                    <th>Ads content</th>
+                </tr>
+            </thead>    	  
+            <tbody>
+            <?php if (count($link_data) > 0): ?>
+            <?php foreach ($link_data as $key => $record): ?>
+                <tr>
+                    <td class="url">  
+                        <?php echo $record['page_url'] ?>
+                    </td>
+                    <td class="link">
+                        <?php echo $record['link_html'] ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>            
+            <?php else:?>
+                <tr>
+                    <td colspan="2" style="text-align:center">No Ads</td>
+                </tr>
+            <?php endif;?>
+            </tbody>        
+        </table> 
+    </div>
+    <div class="tool-box">
+        <form method="post" action="">
+            <input type="hidden" name="update_data" value="1" />
+            <input type="submit" name="submit" value="Refresh Ads" />
+            <?php echo $result_update_text; ?>
+        </form>
+    </div>
+    <? } ?>
+</div>
